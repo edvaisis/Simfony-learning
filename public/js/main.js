8b2f0542ca13b93cf40656002ce365b002182229
edvaisis0 <edvaisis@gmail.com>
2020-11-19 15:11:48 +0000

    Initial commit

---
+++ public/js/main.js
@@ -0,0 +1,15 @@
+
+const artciles = document.getElementById('articles');
+if(artciles) {
+    artciles.addEventListener('click', e => {
+        if (e.target.className === 'btn btn-danger delete-article') {
+            if (confirm('Are you sure')) {
+                const id = e.target.getAttribute('data-id');
+                
+                fetch(`/article/delete/${id}`, {
+                    method: 'DELETE'
+                }).then(res => window.location.reload());
+            }
+        }
+    });
+}
---
+++ src/Entity/Article.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ArticleRepository::class)
+ */
+class Article
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $body;
+
+    
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    public function setBody($body)
+    {
+        $this->body = $body;
+    }
+
+}
